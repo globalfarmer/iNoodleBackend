ff75bf3ee03b164b187087e45bd2580da9e40f34
ABCXYZ\Huy <huyteonb@gmail.com>
2017-05-12 00:53:47 +0700

    #4 fix chuan hoa 2

--- util/dataSlot.js
+++ util/dataSlot.js
@@ -1,55 +1,57 @@
 var slot = require('../models/slot');
 var course = require('../models/course');
 
-module.exports = function(code, term, callback) {
-    var user = {};
-    user.code = code;
-    user.term = term;
+module.exports = function (code, term, callback) {
+	var user = {};
+	user.code = code;
+	user.term = term;
 
-    slot.find({ 'student.code': code, 'course.term': term }, (err, docs) => {
-        if (err)
-            callback(err);
-        var slot = [];
-        var count = 0;
-        if (docs.length == 0) {
-            user.slot = slot;
-            callback(null, user);
-        }
-        docs.forEach((item, index) => {
-            course.findOne({ code: item.course.code, term: term }, (err, cour) => {
-                if (err)
-                    callback(err);
-                if (cour) {
-                    var newSlot = {
-                        student: {
-                            code: docs[index].student.code,
-                            fullname: docs[index].student.fullname,
-                            birthday: docs[index].student.birthday,
-                            klass: docs[index].student.klass
-                        },
-                        course: {
-                            code: docs[index].course.code,
-                            name: docs[index].course.name,
-                            teacher: cour.teacher,
-                            students: cour.students,
-                            dayInWeek: cour.dayInWeek,
-                            session: cour.session,
-                            amphitheater: cour.amphitheater,
-                            group: cour.group,
-                            term: cour.term
-                        },
-                        note: docs[index].note,
-                    }
-                    slot.push(newSlot);
-                } else {
-                    slot.push(docs[index]);
-                }
-                count++;
-                if (count == docs.length) {
-                    user.slot = slot;
-                    callback(null, user);
-                }
-            })
-        });
-    });
+	slot.find({'student.code': code,'course.term': term}, (err, docs) => {
+		if (err)
+			callback(err);
+		var slot = [];
+		var count = 0;
+		if (docs.length == 0){
+			user.slot = slot;
+			callback(null, user);
+		}
+		docs.forEach((item, index) => {
+			if (item.course.code.match(/[A-Z]/g) === null && item.course.code.match(/ /g) === null)
+				course.findOne({code: item.course.code, term: term},(err, cour) => {
+					if (err)
+						callback(err);
+					if (cour){
+						var newSlot = {
+							student: {
+								code: docs[index].student.code,
+								fullname: docs[index].student.fullname,
+								birthday: docs[index].student.birthday,
+								klass: docs[index].student.klass
+							},
+							course: {
+								code: docs[index].course.code,
+								name: docs[index].course.name,
+								teacher: cour.teacher,
+								students: cour.students,
+								dayInWeek: cour.dayInWeek,
+								session: cour.session,
+								amphitheater: cour.amphitheater,
+								group: cour.group,
+								term: cour.term
+							},
+							note: docs[index].note,
+						}
+						slot.push(newSlot);
+					}else{
+						slot.push(docs[index]);
+					}
+					count++;
+					if (count == docs.length){
+						user.slot = slot;
+						callback(null, user);
+					}
+				})
+			else count++;
+		});
+	});
 }
